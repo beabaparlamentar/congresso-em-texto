c609eb72b2887309d9578d681c6de264f1917ee9
Matheus Alves <matheus@odd.group>
2023-02-24 13:00:00 +0100

    Adiciona constantes de URL e de configurações do Scrapy

---
+++ congresso_em_texto/utils/constants/settings.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class SettingsNamespace:
+    DEFAULT = {
+        "USER_AGENT": "Congresso em Texto",
+        "LOG_LEVEL": "ERROR",
+        "DNS_TIMEOUT": 120,
+        "AUTOTHROTTLE_ENABLED": True,
+        "COOKIES_ENABLED": False,
+        "ROBOTSTXT_OBEY": True,
+    }
+
+    def get_default_settings(self):
+        return self.DEFAULT.copy()
+
+    def get_export_settings(self, filepath):
+        settings = self.get_default_settings()
+        settings["FEEDS"] = {f"{filepath}.csv": {"format": "csv", "encoding": "utf-8"}}
+
+        return settings
+
+
+SETTINGS = SettingsNamespace()
---
+++ congresso_em_texto/utils/constants/urls.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class UrlsNamespace:
+    DOMAIN = {
+        "chamber": "https://www.camara.leg.br",
+        "senate": "https://www12.senado.leg.br",
+    }
+    SEARCH = {
+        "chamber": (
+            "https://www.camara.leg.br/internet/sitaqweb/resultadoPesquisaDiscursos.asp"
+        )
+    }
+    SPEECH = {"chamber": "https://www.camara.leg.br/internet/sitaqweb/"}
+
+    def get_domain_url(self, house):
+        return self.DOMAIN.get(house, "")
+
+    def get_search_url(self, house):
+        return self.SEARCH.get(house, "")
+
+    def get_speech_url(self, house):
+        return self.SPEECH.get(house, "")
+
+
+URLS = UrlsNamespace()
