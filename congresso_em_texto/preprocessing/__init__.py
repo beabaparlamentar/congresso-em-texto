9fc910684137977c8f3643cd9e475a234bde80aa
Matheus Alves <matheus@odd.group>
2023-03-08 11:32:30 +0100

    Atualiza estrutura do módulo 'preprocessing'

--- congresso_em_texto/preprocessing/__init__.py
+++ congresso_em_texto/preprocessing/__init__.py
@@ -1,4 +1,5 @@
-from congresso_em_texto.preprocessing.event_preprocessor import EventPreprocessor
-from congresso_em_texto.preprocessing.speech_preprocessor import SpeechPreprocessor
+from congresso_em_texto.preprocessing.event import EventPreprocessor
+from congresso_em_texto.preprocessing.parlamentarian import ParlamentarianPreprocessor
+from congresso_em_texto.preprocessing.speech import SpeechPreprocessor
 
-__all__ = ["EventPreprocessor", "SpeechPreprocessor"]
+__all__ = ["EventPreprocessor", "ParlamentarianPreprocessor", "SpeechPreprocessor"]
