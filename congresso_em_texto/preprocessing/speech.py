a2c854ca812cbb9f8b11877d1431ec9b726a9ea7
Matheus Alves <matheus@odd.group>
2023-03-16 11:30:00 +0100

    Adiciona preprocessador de discursos

--- congresso_em_texto/preprocessing/speech.py
+++ congresso_em_texto/preprocessing/speech.py
@@ -3,4 +3,15 @@ from congresso_em_texto.preprocessing.text import TextPreprocessor
 
 class SpeechPreprocessor(TextPreprocessor):
     def fix(self, speech):
-        pass
+        for key in speech.keys():
+            speech[key] = self.remove_text_escape_chars(speech[key])
+            speech[key] = self.remove_text_doubt_markers(speech[key])
+            speech[key] = self.fix_text_whitespaces(speech[key])
+            speech[key] = self.fix_text_brackets(speech[key])
+
+        speech["orador"] = self.uppercase_text(speech["orador"])
+
+        return speech
+
+    def get_filename(self, event_id):
+        return f"{event_id.replace('/', '-')}.csv"
